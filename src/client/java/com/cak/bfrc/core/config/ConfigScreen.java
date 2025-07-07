5a11433b16ab2c4fce3711740c88fbb906bcbe13
cake <cakeistastydev@gmail.com>
2025-07-07 17:39:45 +0100

    stuffs

--- src/client/java/com/cak/bfrc/core/config/ConfigScreen.java
+++ src/client/java/com/cak/bfrc/core/config/ConfigScreen.java
@@ -3,13 +3,18 @@ package com.cak.bfrc.core.config;
 import com.cak.bfrc.core.BFRC;
 import com.cak.bfrc.core.Lang;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.option.SimpleOptionsScreen;
+import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.option.GameOptions;
 
-public class ConfigScreen extends SimpleOptionsScreen {
+public class ConfigScreen extends GameOptionsScreen {
     
     public ConfigScreen(Screen pLastScreen, GameOptions pOptions) {
-        super(pLastScreen, pOptions, Lang.modTranslatable("gui", "config_screen.title"), ConfigOptions.ALL_OPTIONS);
+        super(pLastScreen, pOptions, Lang.modTranslatable("gui", "config_screen.title"));
+    }
+    
+    @Override
+    protected void addOptions() {
+        this.body.addAll(ConfigOptions.ALL_OPTIONS);
     }
     
     @Override
